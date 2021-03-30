2d3a141d081ea95a6aad4d836f7e8bf213fa44ef
Fredrik Johansen <fredrik1206@gmail.com>
2021-03-30 23:20:21 +0200

    Added comments to the code.

--- CoronaTracker/Form1.cs
+++ CoronaTracker/Form1.cs
@@ -20,6 +20,12 @@ namespace CoronaTracker {
 			InitializeComponent();
 		}
 
+		/// <summary>
+		/// Enables the search WITHOUT date button to work, here is where we grab the search text,
+		/// and make the query to the scraper script. Then returning the result to the format scripts.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
 		private void buttonSearchWithoutDate_Click(object sender, EventArgs e) {
 
 			string inputCountry = textBoxSearch.Text;
@@ -34,20 +40,45 @@ namespace CoronaTracker {
 
 		}
 
+		/// <summary>
+		/// Runs when the global button is pressed. Here will we grab the overall stats from the globe, then returning the data.
+		/// Then formatting the result in the different textboxes.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
 		private void buttonGlobal_Click(object sender, EventArgs e) {
+			// Querying the global stats from the tracker class
 			Tracking result = track.getGlobalStats();
 
+			// Getting the results, then updating the labels with the stats.
 			this.updateTexts(result.countryName, result.newConfirmed, result.totalConfirmed, result.newDeaths, result.totalDeaths, result.newRecovered, result.totalRecovered, result.dateTracked);
+			
+			// Setting the image to a globe, because it's the global stats.
+			// I was not getting this image from the api, because it does not support a globe image.
 			pictureBoxCountry.Image = Properties.Resources.Webp_net_resizeimage;
+			// Then refresing the image to see the new updated image.
 			pictureBoxCountry.Refresh();
 		}
 
-
+		/// <summary>
+		/// Main function that formats the data, and transfering the data to the different labels.
+		/// </summary>
+		/// <param name="countryName">The country's name. If global stats selected, then insert "Global".</param>
+		/// <param name="newConfirmed">The number of new confirmed cases with COVID-19.</param>
+		/// <param name="totalConfirmed">The total of cases confirmed with COVID-19.</param>
+		/// <param name="newDeaths">The number of new deaths in the past 24 hours.</param>
+		/// <param name="totalDeaths">The total number of deaths.</param>
+		/// <param name="newRecovered">The number of recovered persons in the past 24 hours.</param>
+		/// <param name="totalRecovered">The total number of recovered personen.</param>
+		/// <param name="dateTracked">The date the stats were tracked, formatted in the US standard.</param>
 		private void updateTexts(string countryName, int newConfirmed, int totalConfirmed, int newDeaths, int totalDeaths, int newRecovered, int totalRecovered, string dateTracked) {
 
+			// Setting the country label to the selected country name.
 			labelCountry.Text = countryName;
+			// Stripping the time from the date, and placing the "-" with "/"
 			labelDate.Text = dateTracked.ToString().Replace("T", " ").Replace("Z", "").Replace("-", "/").Split('.')[0];
 
+			// Formatting the number stats so the format is: 123.456.
 			labelNewCases.Text = String.Format("{0:n0}", newConfirmed);
 			labelTotalCases.Text = String.Format("{0:n0}", totalConfirmed);
 			labelNewDeaths.Text = String.Format("{0:n0}", newDeaths);
@@ -57,22 +88,36 @@ namespace CoronaTracker {
 
 		}
 
+		/// <summary>
+		/// Getting the image from an api, then updating the image.
+		/// </summary>
+		/// <param name="countryName">The country's name.</param>
+		/// <returns></returns>
 		private bool updateImage(string countryName) {
+			// Getting the country code from the attached file, that contains all the country codes.
 			string countryCode = track.getCountryCode(countryName);
 
+			// If the country code returned "Not Found" then throw an error.
 			if (countryCode == "Not Found") {
 				MessageBox.Show("The country code was not found. \nPlease check your spelling for mistakes", "Country Name Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
 
 				return false;
 			} else {
+				// Updating the image with the image from the api, in the format 64x64 pixels, and the flat format.
 				pictureBoxCountry.Load("https://www.countryflags.io/" + countryCode + "/flat/64.png");
 				
 				return true;
 			}
 		}
 
+		/// <summary>
+		/// Runs when the search button WITH date is clicked.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
 		private void buttonSearchWithDate_Click(object sender, EventArgs e) {
 
+			// Getting the search query from the textbox, then setting the value to the inputCountry.
 			string inputCountry = textBoxSearch.Text;
 
 			// Adding 2 different formats because of api endpoint and visualization.
@@ -82,17 +127,25 @@ namespace CoronaTracker {
 			string startDate = monthCalendar.SelectionRange.Start.ToString("dd/MM/yyyy");
 			string endDate = monthCalendar.SelectionRange.End.ToString("dd/MM/yyyy");
 
-			
+			// Querying the country code, to get the data from the api.
 			Tracking result = track.getRangeStats(inputCountry: textBoxSearch.Text, startDate: startDateAPI, endDate: endDateAPI);
 
+			// Updates the image if the function returns true.
 			if (this.updateImage(countryName: textBoxSearch.Text)) {
+				// Update the labels with the stats.
 				this.updateTexts(result.countryName, result.newConfirmed, result.totalConfirmed, result.newDeaths, result.totalDeaths, result.newRecovered, result.totalRecovered, startDate + " to " + endDate);
 			} else {
+				// If the country was not found, then empty the search text field. The function returns an error code.
 				textBoxSearch.Text = "";
 			}
 
 		}
 
+		/// <summary>
+		/// When the form loads, it sets the max date on the month calender to tommorrow.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
 		private void Form1_Load(object sender, EventArgs e) {
 			monthCalendar.MaxDate = DateTime.Now.AddDays(1);
 		}
--- CoronaTracker/Tracker.cs
+++ CoronaTracker/Tracker.cs
@@ -11,6 +11,9 @@ using System.Windows.Forms;
 
 namespace CoronaTracker {
 
+	/// <summary>
+	/// Making a new object with the relative information received from the api.
+	/// </summary>
 	public class Tracking {
 		public int newRecovered;
 		public int totalRecovered;
@@ -24,10 +27,11 @@ namespace CoronaTracker {
 	}
 
 	class Tracker {
+		// Creating a new web client.
 		WebClient webClient;
 
-
 		public Tracker() {
+			// Setting the security protocols for the webbrowser in the contructor.
 			ServicePointManager.Expect100Continue = true;
 			ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
 
@@ -36,17 +40,24 @@ namespace CoronaTracker {
 			webClient.Encoding = Encoding.UTF8;
 		}
 
-
+		/// <summary>
+		/// Getting the global stats from the api, then returning it in the choosen format.
+		/// </summary>
+		/// <returns>Returning the global stats, which includes 24 hour lookup, and total cases.</returns>
 		public Tracking getGlobalStats() {
+			// Downloads the result from the api endpoint.
 			string apiCall = this.webClient.DownloadString("https://api.covid19api.com/summary");
 
 			Tracking result = new Tracking();
 
+			// Then parsing it into JSON, so it's easier to work with.
 			JsonDocument doc = JsonDocument.Parse(apiCall);
+			// Defining the root of the json document.
 			JsonElement root = doc.RootElement;
 
-			Debug.WriteLine(root.GetProperty("Global"));
+			//Debug.WriteLine(root.GetProperty("Global"));
 
+			// Setting the values of the different stats to the correspondent variabel in the object.
 			result.countryName = "Global";
 			result.dateTracked = root.GetProperty("Global").GetProperty("Date").GetString();
 			result.newConfirmed = root.GetProperty("Global").GetProperty("NewConfirmed").GetInt32();
@@ -59,9 +70,18 @@ namespace CoronaTracker {
 			return result;
 		}
 
+		/// <summary>
+		/// Getting the stats in a selected date range of a country, then returning the result.
+		/// </summary>
+		/// <param name="inputCountry">The searched country's name.</param>
+		/// <param name="startDate">The start date of the date range.</param>
+		/// <param name="endDate">The last date of the date range.</param>
+		/// <returns>Returns the stats of a giving country in a giving date range.</returns>
 		public Tracking getRangeStats(string inputCountry, string startDate, string endDate) {
+			// Creating the result object to store all the stats in.
 			Tracking result = new Tracking();
 
+			// Making some default values, if the api does not return some of the data.
 			int startTotalConfirmed = 0;
 			int endTotalConfirmed = 0;
 			int startTotalDeaths = 0;
@@ -71,39 +91,53 @@ namespace CoronaTracker {
 
 			string apiCall = "";
 
+			// Trying to call the api, if it returns an error, then show an error message to the user.
 			try {
+				// Downloading the result onto the client, then setting the apiCall to the response.
 				apiCall = this.webClient.DownloadString("https://api.covid19api.com/country/" + inputCountry.ToLower().Replace(" ", "-") + "?from=" + startDate + "&to=" + endDate);
 			} catch (Exception) {
 				MessageBox.Show("No stats for that country in that date range. \nPlease choose another date range", "Date Range Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
 				return result;
 			}
 
+			// Parse the result into JSON, so it gets easier to work with.
 			JsonDocument doc = JsonDocument.Parse(apiCall);
+			// Defining the root of the json document.
 			JsonElement root = doc.RootElement;
 
+			// Converting the root of the json document into a JSON element.
 			JsonElement.ArrayEnumerator results = root.EnumerateArray();
 
+			// Looping through the elements in the array of trackings.
 			foreach (var tempResult in results) {
+				// See if the date of the tracking match the start date of the selected range by the client.
 				if (DateTime.Parse(tempResult.GetProperty("Date").GetString()).ToString("yyyy-MM-dd") == startDate) {
+					// If it does, then add it to the total count of confirmed cases, deaths and recovered.
 					startTotalConfirmed += tempResult.GetProperty("Confirmed").GetInt32();
 					startTotalDeaths += tempResult.GetProperty("Deaths").GetInt32();
 					startTotalRecovered += tempResult.GetProperty("Recovered").GetInt32();
 					continue;
+				// See if the date of the tracking matches the end date of the selected range by the client.
 				} else if (DateTime.Parse(tempResult.GetProperty("Date").GetString()).ToString("yyyy-MM-dd") == endDate) {
+					// If it does, then add it to the total count of confirmed cases, deaths and recovered.
 					endTotalConfirmed += tempResult.GetProperty("Confirmed").GetInt32();
 					endTotalDeaths += tempResult.GetProperty("Deaths").GetInt32();
 					endTotalRecovered += tempResult.GetProperty("Recovered").GetInt32();
 					continue;
+				// Otherwise if the tracking does not match either of start or end date, then continue.
 				} else {
 					continue;
 				}
 			}
 
+			// Calculting the total of new confirmed, deaths and recovered personen in the selected period.
 			int totalConfirmed = endTotalConfirmed - startTotalConfirmed;
 			int totalDeaths = endTotalDeaths - startTotalDeaths;
 			int totalRecovered = endTotalRecovered - startTotalRecovered;
 
+			// Setting the stats into the result object.
 			result.countryName = inputCountry;
+			// Format the date to a range, so it gets more readable.
 			result.dateTracked = startDate + " to " + endDate;
 			result.newConfirmed = 0;
 			result.newDeaths = 0;
@@ -116,23 +150,36 @@ namespace CoronaTracker {
 			return result;
 		}
 
+		/// <summary>
+		/// Gets the overall stats of a country.
+		/// </summary>
+		/// <param name="inputCountry">The name of the searched country.</param>
+		/// <returns>Returns the stats of the country which includes a 24 hour overview, and total stats.</returns>
 		public Tracking getOverallStats(string inputCountry) {
 
+			// Creating the new JsonElement, which includes a list of all the countries.
 			JsonElement resultCountry = new JsonElement();
+			// Creating the result tracking object.
 			Tracking returnResult = new Tracking();
 
+			// Downloading the result from the api.
 			string apiCall = this.webClient.DownloadString("https://api.covid19api.com/summary");
 
+			// Parsing the data into a JsonDocument.
 			JsonDocument doc = JsonDocument.Parse(apiCall);
 			JsonElement root = doc.RootElement;
 
-
+			// Making the JsonDocument into a array which we can loop through.
 			JsonElement.ArrayEnumerator countries = root.GetProperty("Countries").EnumerateArray();
 
+			// Loop through every country in the list of countries.
 			foreach (var country in countries) {
+				// Checkes if the name of the search country matches the the looped country's name.
 				if (country.GetProperty("Slug").GetString() == inputCountry.ToLower().Replace(" ", "-")) {
+					// If it does, then setting the result country to the looped country.
 					resultCountry = country;
 					break;
+				// Else continue the loop.
 				} else {
 					continue;
 				}
@@ -143,6 +190,7 @@ namespace CoronaTracker {
 				return returnResult;
 			}
 
+			// Binding the stats to the result object, then returing the result object.
 			returnResult.countryName = inputCountry;
 			returnResult.dateTracked = resultCountry.GetProperty("Date").GetString();
 			returnResult.totalConfirmed = resultCountry.GetProperty("TotalConfirmed").GetInt32();
@@ -155,21 +203,32 @@ namespace CoronaTracker {
 			return returnResult;
 		}
 
+		/// <summary>
+		/// Gets the correspondent country code to the inputted country.
+		/// </summary>
+		/// <param name="countryName">The name of the country.</param>
+		/// <returns>The country code of the giving country.</returns>
 		public string getCountryCode(string countryName) {
+			// Getting the path of the included file which contains a list of countries.
 			string path = Path.Combine(Environment.CurrentDirectory, @"..\..");
 			var filename = Path.Combine(path, "CountryCode.json");
 
 			string countryCode = "Not Found";
 
+			// If the file exists, then continue.
 			if (File.Exists(filename)) {
+				// Reading the files content.
 				string textContent = File.ReadAllText(filename);
 
+				// Parsing the content of the file into JSON.
 				JsonDocument doc = JsonDocument.Parse(textContent);
 				JsonElement root = doc.RootElement;
 
 				JsonElement countryList = root.GetProperty("countries");
 
+				// Looping through the list of country codes.
 				foreach (var item in countryList.EnumerateArray()) {
+					// If the looped country's name matches the inputted country's name, then setting the looped items country code to a variable named country code
 					if (item.GetProperty("name").GetString() == countryName) {
 						countryCode = item.GetProperty("code").GetString();
 						break;
@@ -177,6 +236,7 @@ namespace CoronaTracker {
 						continue;
 					}
 				}
+			// Throws an error message if the file was not found.
 			} else {
 				MessageBox.Show("The country list file does not exist in the current project. \n Contact Fredrik Johansen for the fix.", "File Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
 			}
